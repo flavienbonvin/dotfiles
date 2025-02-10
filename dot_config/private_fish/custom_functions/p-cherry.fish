6c0d56583a268a3d5d805dbe97f39fc0b16b0ce8
Flavien Bonvin <flavien.bonvin@pm.me>
2025-02-10 09:46:34 +0100

    Simplify cherry pick
    Simplify cherry pick

--- dot_config/private_fish/custom_functions/p-cherry.fish
+++ dot_config/private_fish/custom_functions/p-cherry.fish
@@ -3,9 +3,8 @@ function __fish_my_git_function_branches
 end
 
 function p-cherry
-    printf '%s' $PWD (fish_git_prompt) ' $ '
     set branch $argv[1]
+
     git checkout $branch
     git fetch && git reset --hard origin/$branch
-    yarn install
 end
