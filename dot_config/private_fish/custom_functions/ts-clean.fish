d46672226834dc28aaffad8896fccf4a82fecf6b
Flavien Bonvin <flavien.bonvin@pm.me>
2025-05-15 11:49:24 +0200

    Add ts-clean method

---
+++ dot_config/private_fish/custom_functions/ts-clean.fish
@@ -0,0 +1,13 @@
+function ts-clean -d 'Clean typescript caches'
+    echo "Cleaning typescript caches"
+    rm -rf .turbo
+    rm -f .eslintcache
+
+    if not string match -q "/Users/fbonvin/Developer/*" (pwd)
+        echo "You're not in a dev folder"
+        return 1
+    end
+
+    echo "Cleaning any .cache folder"
+    find . -type d -name ".cache" -exec rm -rf {} +
+end
