5ca620c1147392cb8a37fd67c1fcb5ebbed0ce48
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-02-16 08:38:18 +0100

    Update app store app IDs

--- packages/fish-work/.config/fish/config.fish
+++ packages/fish-work/.config/fish/config.fish
@@ -4,3 +4,6 @@ set -x NODE_EXTRA_CA_CERTS (mkcert -CAROOT)/rootCA.pem
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# opencode
+fish_add_path /Users/fbonvin/.opencode/bin
