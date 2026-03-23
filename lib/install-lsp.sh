ed5485b51d7837595a5714b640325d7c826c4ce9
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-03-23 12:59:55 +0100

    Add prettier formatter for JS, TS and TSX

--- lib/install-lsp.sh
+++ lib/install-lsp.sh
@@ -4,5 +4,6 @@ bun install -g \
       typescript \
       typescript-language-server \
       @tailwindcss/language-server \
+      vscode-langservers-extracted \
       @astrojs/language-server \
       prettier
