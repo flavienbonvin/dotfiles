46d85f222c6f178e37fa5d961b3ee17cb126a6e4
Flavien Bonvin <flavien.bonvin@pm.me>
2026-05-13 22:41:29 +0200

    Small helix and zed update

--- packages/fish-personal/.config/fish/config.fish
+++ packages/fish-personal/.config/fish/config.fish
@@ -7,3 +7,10 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 set -gx PATH $PATH /Users/fbonvin/.lmstudio/bin
+
+# pnpm
+set -gx PNPM_HOME "/Users/fbonvin/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
